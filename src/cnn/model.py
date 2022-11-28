20a19a9b6c27b902f1507fec84739071cf3f9c2b
Marcel Friedrich <waynearne1337@googlemail.com>
2022-11-28 08:44:31 +0100

    Restructure code, model outsourcing

---
+++ src/cnn/model.py
@@ -0,0 +1,71 @@
+from typing import Tuple
+
+import tensorflow as tf
+
+from tensorflow.keras.layers import Conv2D, Activation, Dense, Flatten, MaxPool2D, Input, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+
+
+def build_model(
+    img_shape: Tuple[int, int, int],
+    num_classes: int,
+    learning_rate: float,
+    conf_filter_1: int,
+    kernel_size_1: int,
+    conf_filter_2: int,
+    kernel_size_2: int,
+    conf_filter_3: int,
+    kernel_size_3: int,
+    dense_layer_size: int,
+    activation: str,
+    optimizer: tf.keras.optimizers.Optimizer,
+    use_batch_normalization: bool = False
+) -> Model:
+    input_img = Input(shape=img_shape)
+
+    """ Conv-Block-1"""
+    x = Conv2D(filters=conf_filter_1, kernel_size=kernel_size_1, padding="same")(input_img)
+    x = Activation(activation)(x)
+    x = Conv2D(filters=conf_filter_1, kernel_size=kernel_size_1, padding="same")(x)
+    if use_batch_normalization:
+        x = BatchNormalization()(x)
+    x = Activation(activation)(x)
+    x = MaxPool2D()(x)
+
+    """ Conv-Block-2"""
+    x = Conv2D(filters=conf_filter_2, kernel_size=kernel_size_2, padding="same")(x)
+    x = Activation(activation)(x)
+    x = Conv2D(filters=conf_filter_2, kernel_size=kernel_size_2, padding="same")(x)
+    if use_batch_normalization:
+        x = BatchNormalization()(x)
+    x = Activation(activation)(x)
+    x = MaxPool2D()(x)
+
+    """ Conv-Block-3"""
+    x = Conv2D(filters=conf_filter_3, kernel_size=kernel_size_3, padding="same")(x)
+    x = Activation(activation)(x)
+    x = Conv2D(filters=conf_filter_3, kernel_size=kernel_size_3, padding="same")(x)
+    if use_batch_normalization:
+        x = BatchNormalization()(x)
+    x = Activation(activation)(x)
+    x = MaxPool2D()(x)
+
+    """ Convert to Vector for Dense-Layer"""
+    x = Flatten()(x)
+    x = Dense(units=dense_layer_size)(x)
+    if use_batch_normalization:
+        x = BatchNormalization()(x)
+    x = Activation(activation)(x)
+
+    """ One-Hot output"""
+    x = Dense(units=num_classes)(x)
+    y_pred = Activation("softmax")(x)
+
+    model = Model(inputs=[input_img], outputs=[y_pred])
+
+    model.compile(
+        loss="categorical_crossentropy", optimizer=optimizer(learning_rate), metrics=["accuracy"]
+    )
+
+    return model
--- src/main.py
+++ src/main.py
@@ -1,70 +1,60 @@
 import os
+import shutil
 
-from typing import Tuple
-from Cifar10 import Cifar10
+from cnn.Cifar10 import Cifar10
+from cnn.model import build_model
 
-from tensorflow.keras.layers import Conv2D, Activation, Dense, Flatten, MaxPool2D, Input
-from tensorflow.keras.callbacks import TensorBoard
+from scipy.stats import randint, uniform
+import numpy as np
+
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import Model
+from sklearn.model_selection import ParameterSampler
 
 LOGS_DIR = os.path.join(os.path.dirname(__file__), 'logs')
+EPOCHS = 20
+RAND_SEARCH_N_TO_CALC = 10
+BATCH_SIZE = 128
+DATA = Cifar10()
+NUM_CLASSES = 10
+
+"""
+    Initial try and testing out parameter selection.
+"""
 
 
-def build_model(img_shape: Tuple[int, int, int], num_classes: int) -> Model:
-    input_img = Input(shape=img_shape)
+def train_initial_model():
+    tb_callback = TensorBoard(log_dir=os.path.join(LOGS_DIR, 'model_initial'), histogram_freq=0, profile_batch=0)
+    train_dataset = DATA.get_train_set()
+    val_dataset = DATA.get_val_set()
 
-    x = Conv2D(filters=32, kernel_size=2, padding="same")(input_img)
-    x = Activation("relu")(x)
-    x = Conv2D(filters=32, kernel_size=2, padding="same")(x)
-    x = Activation("relu")(x)
-    x = MaxPool2D()(x)
+    hyper_params = {
+        "img_shape": DATA.img_shape,
+        "num_classes": NUM_CLASSES,
+        "learning_rate": 0.001,
+        "conf_filter_1": 32,
+        "kernel_size_1": 2,
+        "conf_filter_2": 48,
+        "kernel_size_2": 2,
+        "conf_filter_3": 64,
+        "kernel_size_3": 2,
+        "dense_layer_size": 128,
+        "activation": "relu",
+        "optimizer": Adam
+    }
 
-    x = Conv2D(filters=48, kernel_size=2, padding="same")(x)
-    x = Activation("relu")(x)
-    x = Conv2D(filters=48, kernel_size=2, padding="same")(x)
-    x = Activation("relu")(x)
-    x = MaxPool2D()(x)
-
-    x = Conv2D(filters=64, kernel_size=2, padding="same")(x)
-    x = Activation("relu")(x)
-    x = Conv2D(filters=64, kernel_size=2, padding="same")(x)
-    x = Activation("relu")(x)
-    x = MaxPool2D()(x)
-
-    x = Flatten()(x)
-    x = Dense(units=128)(x)
-    x = Activation("relu")(x)
-    x = Dense(units=num_classes)(x)
-    y_pred = Activation("softmax")(x)
-
-    model = Model(inputs=[input_img], outputs=[y_pred])
-
-    model.compile(
-        loss="categorical_crossentropy", optimizer=Adam(), metrics=["accuracy"]
+    model = build_model(**hyper_params)
+    model.fit(
+        x=train_dataset,
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        callbacks=[tb_callback],
+        validation_data=val_dataset
     )
-
-    return model
 
 
 if __name__ == '__main__':
     if not os.path.exists(LOGS_DIR):
         os.mkdir(LOGS_DIR)
 
-    epochs = 20
-    batch_size = 128
-    data = Cifar10()
-
-    train_dataset = data.get_train_set()
-    val_dataset = data.get_val_set()
-    test_dataset = data.get_test_set()
-
-    tb_callback = TensorBoard(log_dir=os.path.join(LOGS_DIR, 'model_initial'), histogram_freq=0, profile_batch=0)
-    model = build_model(img_shape=data.img_shape, num_classes=10)
-    model.fit(
-        x=train_dataset,
-        batch_size=batch_size,
-        epochs=epochs,
-        callbacks=[tb_callback],
-        validation_data=val_dataset
-    )
+    train_initial_model()
